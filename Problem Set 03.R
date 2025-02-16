65920a95353702dd4fdd500a673ca942c3b15678
Will Hopper <wjhopper510@gmail.com>
2025-02-16 16:24:51 -0500

    Added instructor solutions

--- Problem Set 03.R
+++ Problem Set 03.R
@@ -19,6 +19,22 @@ scatterplot <- function() {
 # Note that the names of the x and y variables must be passed to the scatterplot() function as
 # character vectors.
 
+#### Instructor Solution ####
+
+library(ggplot2)
+
+scatterplot <- function(data, x, y, ...) {
+  ggplot2::ggplot(
+    data = data,
+    mapping = aes(.data[[x]], .data[[y]])
+  ) +
+    geom_point(...)
+}
+
+## Example usage
+scatterplot(data = mtcars, x = "hp", y = "mpg", color = "red", size = 4)
+scatterplot(data = mtcars, x = "hp", y = "mpg", color = "red", size = 10, stroke = 4, pch = 9)
+
 ## Exercise 2
 
 # The Open Notify API allows you to retrieve real-time data about the astronauts currently in space.
@@ -33,8 +49,9 @@ current_space <- jsonlite::read_json("http://api.open-notify.org/astros.json")
 # your function should return a scalar character vector holding the name of the spacecraft this
 # astronaut is aboard.  If a match is *not* found in the list, your function should NA.
 
-# Your function can assume the list returned from the Open Notify API will have the following
-# structure:
+# Your function may assume that the user will input the astronaut's name as a scalar character
+# vector, and your function may assume the list returned from the Open Notify API will have the
+# following structure:
 
 # List of 3
 #  $ people : List of *N*
@@ -44,9 +61,32 @@ current_space <- jsonlite::read_json("http://api.open-notify.org/astros.json")
 #  $ number : int *N*
 #  $ message: chr "success"
 
-# Your function should *not* assume that the user will input the astronaut's name as a scalar
-# character vector, and should throw an error if they do not. The accuracy of your function should
-# also not depend on there being exactly 12 astronauts in space.
+# However,the accuracy of your function should not depend on there being exactly 12 astronauts in
+# space.
+
+#### Instructor Solution ####
+
+find_craft <- function(space_list, astronaut) {
+
+  if (typeof(astronaut) != "character" || length(astronaut) != 1L) {
+    stop("The `astronaut` argument should be a scalar character vector")
+  }
+
+  craft <- NA
+
+  for (person in space_list$people) {
+
+    if (person$name == astronaut) {
+      craft <- person$craft
+    }
+  }
+
+  return(craft)
+}
+
+# Example usage
+find_craft(space_list = current_space, astronaut = "Sunita Williams")
+find_craft(space_list = current_space, astronaut = "Will Hopper")
 
 #### Exercise 3 ####
 
@@ -60,3 +100,20 @@ current_space <- jsonlite::read_json("http://api.open-notify.org/astros.json")
 
 # Make sure that, like the sum() function, your subtract() function allows the user to remove
 # missing values from the vector before computing.
+
+#### Instructor Solution
+
+subtract <- function(x, na_rm = TRUE) {
+
+  if (na_rm) {
+    x <- x[!is.na(x)]
+  }
+
+  y <- x[1]
+  for (v in x[-1]){
+    y <- y - v
+  }
+  return(y)
+}
+
+subtract(c(2, 10, -2, 5, NA), na.rm = TRUE)
